7e7887c9fcb9185ac04111727dc5caf7e00ad8d2
go <misong321@gmail.com>
2021-05-30 18:32:15 +0900

    [feat]add project and contact info

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ window.addEventListener("scroll", function () {
     banner.style.marginBottom = value * -0.5 + "px";
   }
 });
-// category click event
-const category = document.getElementsByTagName("h2");
-const ul = document.querySelector("body > nav > ul");
-ul.addEventListener("click", function(e) {
-    const idx = e.currentTarget.indexOf(e.target);
-    console.log(e.target.parentNode);
-    console.log(idx);
-})
+// nav bar click event
+// const ul = document.querySelector("body > nav > ul");
+// const sections = document.querySelectorAll("body > section");
+// ul.addEventListener("click", function(e) {
+//   const category = e.currentTarget.children;
+//     const idx = [...category].findIndex(elem=>elem === e.target);
+//       sections[idx].scrollIntoView({behavior:'smooth'});
+//   })
 // skills hover effect
 const container = document.getElementById("skillsWrap");
 const desc = document.getElementsByClassName("detailDesc");
@@ -45,13 +45,13 @@ container.addEventListener("mouseover", function (e) {
         content = `Semantic HTML 필요성에 공감하며 태그에 의미를 담아 작성하려 합니다. UI구현에 필요한 영역 구분을 빠르게 해낼 수 있습니다.`;
         break;
       case "CSS":
-        content = `<span>부모-자식 요소간 영향</span>을 고려할 줄 압니다. CSS in JS를 주로 사용하였으나 pure CSS 연습을 위해 <span>포트폴리오 사이트는 pure CSS로 구현</span>했습니다.`;
+        content = `<span>적용 우선순위</span>을 고려할 줄 압니다. CSS in JS를 주로 사용하였으나 pure CSS 연습을 위해 <span>포트폴리오 사이트는 pure CSS로 구현</span>했습니다.`;
         break;
       case "Styled Components":
         content = `React 컴포넌트와 함께 <span>오버라이딩, props를 통한 조건부 적용</span>을 구현할 수 있습니다. <span>5차례의 프로젝트</span>에 사용했습니다.`;
         break;
       case "SCSS":
-        content = `css-sass-scss를 구분할 수 있습니다. 특히 <span>Variables, Nesting</span>의 편리함에 공감하고 사용할 수 있습니다.`;
+        content = `css-sass-scss 코드를 구분할 수 있습니다. 특히 <span>Variables, Nesting</span>의 편리함에 공감하고 사용할 수 있습니다.`;
         break;
         case "Python":
             content = `<span>70여개의 알고리즘</span> 문제를 풀며 Python문법을 익혔습니다. <span>웹 스크래핑 및 ajax</span>를 이용해 통신하는 서버를 구축한 적이 있습니다.`;
@@ -63,7 +63,7 @@ container.addEventListener("mouseover", function (e) {
             content = `Realtime DB, Cloud storage, Authentication를 이용하여 <span>BaaS 프로젝트를 2회</span> 진행했습니다.`;
         break;
         case "Git":
-            content = `3차례의 팀프로젝트 과정에서 <span>git을 이용하여 협업</span>을 진행했고 가장 최근에는 커밋메시지 컨벤션을 정해 사용하기도 했습니다. `;
+            content = `개인 소스코드 관리 및 <span>Git을 이용한 협업을 3차례</span> 진행했고 커밋메시지 컨벤션을 정해 사용해보았습니다. `;
         break;
       default:
         break;
@@ -129,6 +129,61 @@ const config = {
   },
 };
 const myChart = new Chart(document.getElementById("myChart"), config);
+
+
+//send email
+const sendMessage = document.querySelector("#CONTACT > div > div > div.email.flex > button");
+sendMessage.addEventListener("click", function(){
+const name = document.getElementById("inputName").value;
+const email = document.getElementById("inputEmail").value;
+const message = document.getElementById("inputMessage").value;
+if(name.trim() && email.trim() && message.trim()){
+  const regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
+  if(email.match(regExp)){
+    emailjs.send("service_9yyi6ph","template_hvxrza2",{
+      from_name: name,
+      email: email,
+      message: message,
+      }).then(
+        function (response) {
+          Swal.fire({
+            title: '전송완료!',
+            text: '빠른 시일 내 답장드리겠습니다 :)',
+            confirmButtonText: 'OK',
+            backdrop: `
+          rgba(0,0,123,0.4)
+          url("images/cat.gif")
+          left center
+          no-repeat
+        `
+          })
+        },
+        function (error) {
+          Swal.fire({
+            icon: 'error',
+            text: '전송에 실패하였습니다.',
+            footer: '<a href="mailto:mygomi05@gmail.com">직접 전송하기</a>'
+          })
+        }
+      )
+    
+  }else{
+    Swal.fire({
+      icon: 'error',
+      text: '이메일 형식을 확인해주세요!',
+      footer: '<a href="mailto:mygomi05@gmail.com">직접 전송하기</a>'
+    })
+  }
+}else{
+  Swal.fire({
+    icon: 'error',
+    text: '정보를 모두 입력해주세요!',
+  })
+}
+
+
+})
+
 
 // page up
 const upBtn = document.getElementById("upArrow");
